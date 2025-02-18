9d319af36ebfd652b9d7e433f17d60ec62334136
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-02-18 09:53:38 +0100

    set up config server

---
+++ config-server/src/test/java/ma/hmzelidrissi/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package ma.hmzelidrissi.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
