8dd05700f364267de0804bfb2b7272de24fe2b29
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2025-02-19 14:39:25 +0100

    setup database and create entities

---
+++ stock-service/src/main/java/com/jee/stockservice/entity/Category.java
@@ -0,0 +1,19 @@
+package com.jee.stockservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+}
---
+++ stock-service/src/main/java/com/jee/stockservice/entity/Stock.java
@@ -0,0 +1,22 @@
+package com.jee.stockservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
