2fe9cec32604b768fc410f70edc194dfe9fc2239
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-03-03 12:21:28 +0000

    clean up

--- payment-service/src/main/java/ma/hmzelidrissi/paymentservice/repository/PaymentRepository.java
+++ payment-service/src/main/java/ma/hmzelidrissi/paymentservice/repository/PaymentRepository.java
@@ -1,11 +1,9 @@
 package ma.hmzelidrissi.paymentservice.repository;
 
+import java.util.List;
 import ma.hmzelidrissi.paymentservice.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
